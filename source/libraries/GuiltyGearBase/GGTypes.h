b046d482ed58e40f42e3b9fe99ccbbd8f90f374c
lovenus <124386022+MoonSquaredd@users.noreply.github.com>
2025-04-16 09:41:00 -0300

    Create GGTypes.h

---
+++ source/libraries/GuiltyGearBase/GGTypes.h
@@ -0,0 +1,18 @@
+#ifndef GGTypes
+#define GGTypes
+
+#include <windows.h>
+
+typedef struct {
+	int flag;
+	int x;
+	int y;
+	int tx;
+	int ty;
+	unsigned short actno;
+	short Localid;
+	unsigned short angle;
+	unsigned short padd;
+} CREATE_ARG;
+
+#endif
