ed5069c4b4db4f069c020e769a981d11c798fa17
lovenus <124386022+MoonSquaredd@users.noreply.github.com>
2025-04-17 04:10:41 -0300

    Update GG.h

--- source/libraries/GuiltyGearBase/GG.h
+++ source/libraries/GuiltyGearBase/GG.h
@@ -12,6 +12,13 @@ static inline BYTE *GetBase() {
 	return (BYTE *)GetModuleHandle(NULL);
 }
 
+static inline void PatchSafe(void *dest,const void *src,int size) {
+	DWORD oldProtect;
+	VirtualProtect(dest,size,PAGE_EXECUTE_READWRITE,&oldProtect);
+	memcpy(dest,src,size);
+	VirtualProtect(dest,size,oldProtect,&oldProtect);
+}
+
 static inline void AssignToGameLoop(VoidFunc func) {
 	HMODULE hook = GetModuleHandleA("DBGHELP.DLL");
 	original ATGP = (original)GetProcAddress(hook, "AssignToGameLoop");
